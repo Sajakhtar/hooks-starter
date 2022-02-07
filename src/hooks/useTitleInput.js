f678b1004454262996e323fd06db9f24eafc0dee
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2022-02-07 22:01:13 +0400

    useDebugvalue as a customized way to test something and see the result in the React Dev Tools

--- src/hooks/useTitleInput.js
+++ src/hooks/useTitleInput.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useDebugValue } from 'react';
 
 function useTitleInput(initialValue) {
   const [ value , setValue ] = useState(initialValue)
@@ -7,6 +7,9 @@ function useTitleInput(initialValue) {
     // set page title to name state
     document.title = value;
   });
+
+  // A customized way to test something and see the result in the React Dev Tools
+  useDebugValue(value.length > 0 ? 'full' : 'empty');
 
   return [ value, setValue ];
 }
