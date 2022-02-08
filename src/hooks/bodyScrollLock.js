a594b7f4fcc3d154da405f2a88ce8e54dcdc2f29
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2022-02-08 12:19:31 +0400

    cleanup (on un-mount) useBodyScrollLock when toggled off

--- src/hooks/bodyScrollLock.js
+++ src/hooks/bodyScrollLock.js
@@ -4,8 +4,18 @@ import { useLayoutEffect } from "react";
 
 function useBodyScrollLock() {
   useLayoutEffect( () => {
+    const originalOverflow = window.getComputedStyle(document.body).overflow;
     document.body.style.overflow = 'hidden';
-  })
+
+    //  Return a function from the hook that will run on un-mount
+    // return function cleanup() {
+    //   document.body.style.overflow = '';
+    // }
+
+    return () => {
+      document.body.style.overflow = originalOverflow;
+    }
+  }, [])
 }
 
 
